05890bc86aed9dba38df04ed524466c3de028b7f
DariaKliukhina <dariasergeevna90@gmail.com>
2018-02-16 16:25:21 +0300

    Скрипты, карта и мелкие правки

--- source/js/script.js
+++ source/js/script.js
@@ -4,32 +4,37 @@ var menugroup2 = document.querySelector(".main-nav__group--2");
 var link = document.querySelector(".week-product__buy");
 var products = document.querySelector(".products");
 var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal__add");
+var close = document.querySelector(".modal__add");
 var overlay = document.querySelector(".modal__overlay");
-products.addEventListener("click", function (evt) {
-  if (evt.target.classList.contains("product__icon")){
+if(products) {
+  products.addEventListener("click", function (evt) {
+    if (evt.target.classList.contains("product__icon")) {
+      popup.classList.add("modal__show");
+      overlay.classList.add("modal__overlay-show");
+      user.focus();
+    }
+  });
+}
+if(link) {
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
     popup.classList.add("modal__show");
     overlay.classList.add("modal__overlay-show");
     user.focus();
-  }
-});
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  alert("test");
-  popup.classList.add("modal__show");
-  overlay.classList.add("modal__overlay-show");
-  user.focus();
-});
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal__show");
-  overlay.classList.remove("modal__overlay-show")
-});
-overlay.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal__show");
-  overlay.classList.remove("modal__overlay-show")
-});
+  });
+}
+if(popup){
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal__show");
+    overlay.classList.remove("modal__overlay-show")
+  });
+  overlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal__show");
+    overlay.classList.remove("modal__overlay-show")
+  });
+}
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("modal__show")) {
